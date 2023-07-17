c470c8c887cafbd434d7f20f281ae88ebde62666
Nath <playerxmatt@gmail.com>
2023-07-16 21:34:36 -0300

    Modified Credito and Debito classes and it's interaction with Conta class through the sacar() and depositar() methods

--- 03-TYPESCRIPT/src/Conta.ts
+++ 03-TYPESCRIPT/src/Conta.ts
@@ -1,23 +1,59 @@
-export default abstract class Conta{
+export default abstract class Conta {
+  private _saldo: number;
+
   constructor(
-    private readonly _numero:string
-  ){}
+    private readonly _numero: string,
+    private _limite: number,
+    private _creditos: Credito[] = [],
+    private _debitos: Debito[] = [],
+  ) {
+    this._saldo = 0;
+  }
 
-  abstract depositar(valor: number): void;
-  abstract sacar(valor: number): void;
+  public depositar(valor: number): void {
+    this._saldo += valor;
+    this._creditos.push(new Credito(valor))
+  }
 
-  public get numero () : string { return this._numero; }
+  public sacar(valor: number): void {
+    this._saldo -= valor;
+    this._debitos.push(new Debito(valor))
+  }
+
+  public get numero(): string { return this._numero; }
+  public get saldo () : number { return this._saldo; }
+
+  public get limite(): number { return this._limite; }
+  public set limite(v: number) { this._limite = v; }
+
 }
 
+
+// Classes auxiliares para transferência bancária
+
 class Credito {
-  constructor(private _valor: number) {}
-  public get valor() : number  { return this._valor; }
-  public set valor(v : number) { this._valor = v; }
+  private readonly _data: Date;
+
+  constructor(
+    private readonly _valor: number
+  ) {
+    this._data = new Date();
+  }
+
+  public get valor(): number { return this._valor; }
+  public get data(): Date { return this._data; }
 
 }
 
 class Debito {
-  constructor(private _valor: number) {}
-  public get valor() : number  { return this._valor; }
-  public set valor(v : number) { this._valor = v; }
+  private readonly _data: Date;
+
+  constructor(
+    private readonly _valor: number
+  ) {
+    this._data = new Date();
+  }
+
+  public get valor(): number { return this._valor; }
+  public get data(): Date { return this._data; }
 }

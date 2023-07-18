abfa28d26d083ab2ad1e8d317133ddbffaa71084
Nath <playerxmatt@gmail.com>
2023-07-17 23:50:52 -0300

    Added second application

---
+++ 03-TYPESCRIPT/src/aplicacao2.ts
@@ -0,0 +1,43 @@
+import Cliente from "./Cliente";
+import Endereco from "./Endereco";
+
+var cliente: Cliente = new Cliente(
+  "123.123.123-12",
+  "Claudio Júnior",
+  "(88) 99999-9999"
+);
+
+cliente.adicionarEndereco(
+  new Endereco(
+    "63200-000",
+    "Rua Jhonatan da Nova Geração",
+    "139 B",
+    "APTO. 3",
+    "Caririaçu",
+    "Ceará"
+  )
+);
+
+cliente.adicionarEndereco(
+  new Endereco(
+    "63200-000",
+    "Rua Jhonatan da Nova Geração",
+    "139 C",
+    "APTO. 2",
+    "Caririaçu",
+    "Ceará"
+  )
+);
+
+cliente.adicionarEndereco(
+  new Endereco(
+    "63200-000",
+    "Rua Olavo de Carvalho",
+    "120",
+    "Ao lado do mercadinho Ojuara",
+    "Caririaçu",
+    "Ceará"
+  )
+);
+
+cliente.listarEnderecos();

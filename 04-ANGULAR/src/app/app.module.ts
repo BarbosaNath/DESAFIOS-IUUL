1832c245566d8c74f2e6fd7bd231c05dbb5078c6
Nath <playerxmatt@gmail.com>
2023-07-31 10:35:23 -0300

    Fixed removed component error

--- 04-ANGULAR/src/app/app.module.ts
+++ 04-ANGULAR/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -13,7 +12,6 @@ import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent,
     HomeComponent
   ],
   imports: [
